734283b254eb584951c2da604f0a6fd2f9a9f2df
AvihayR <avihayrabi@gmail.com>
2025-03-01 19:02:05 +0200

    Change Header content

--- cmps/Header.jsx
+++ cmps/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="min-w-full text-sm md:text-md m-2.5 mb-8 p-1 text-amber-200">
-            <h2 className="text-2xl">Welcome 👋</h2>
+            <h2 className="text-2xl">Welcome! 👋</h2>
         </header>
     )
 }
