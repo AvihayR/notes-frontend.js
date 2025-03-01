9cd709509c7755476bb3e6d168f85fd8a024ed35
AvihayR <avihayrabi@gmail.com>
2025-03-01 23:37:39 +0200

    Add SubNote cmp

--- cmps/BigNote.jsx
+++ cmps/BigNote.jsx
@@ -1,14 +1,22 @@
+import SubNote from "./SubNote";
+
 export default function BigNote() {
     return (
         <div className="big-note w-100 md:w-150 h-180 p-1.5">
             <div className="horizontal-container w-full h-full flex bg-amber-200 rounded-lg relative">
-                <div className="note-header text-lg md:text-2xl font-medium text-amber-400 border-b-blue-200 border-b-2 w-full h-18 flex justify-center items-center absolute">
+                <div className="note-header z-10 bg-amber-200 text-md md:text-2xl font-extralight text-amber-400 border-b-blue-200 border-b-1 w-full h-18 flex justify-center items-center absolute">
                     Avihay's Notebook 📝
                 </div>
-                <div className="left-container border-r-1 border-r-pink-400 w-14 h-full relative"></div>
+                <div className="left-container z-20 border-r-2 border-r-pink-400 w-16 h-full relative"></div>
+                <div className="right-container overflow-y-scroll w-full h-full pt-20 absolute">
+                    <SubNote />
+                    <SubNote />
+                    <SubNote />
+                    <SubNote />
+                    <SubNote />
+                    <SubNote />
+                </div>
             </div>
-            {/* <div className="note-header caret-amber-100 bg-amber-200 w-full h-8 rounded-tl-lg rounded-tr-lg"></div> */}
-            {/* caret-amber-100 bg-amber-200 rounded-lg */}
         </div>
     )
 }
---
+++ cmps/SubNote.jsx
@@ -0,0 +1,7 @@
+export default function SubNote() {
+    return (
+        <div className="sub-note-container h-18 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
+            subnote text goes here - the brown Joey jumps over the white fence
+        </div>
+    )
+}
