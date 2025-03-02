82e028879714a51e2ad195d967fe484ed4047ebe
AvihayR <avihayrabi@gmail.com>
2025-03-02 21:48:30 +0200

    Improve overall application styling

--- cmps/SubNote.jsx
+++ cmps/SubNote.jsx
@@ -1,6 +1,6 @@
 export default function SubNote() {
     return (
-        <div className="sub-note-container h-18 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
+        <div className="sub-note-container h-19 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
             subnote text goes here - the brown Joey jumps over the white fence
         </div>
     )
