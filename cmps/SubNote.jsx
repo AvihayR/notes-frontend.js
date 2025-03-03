63f0cb06444ac092cf045d132a08745c9733cdab
AvihayR <avihayrabi@gmail.com>
2025-03-04 00:03:32 +0200

    Use Get (single) REST API endpoint

--- cmps/SubNote.jsx
+++ cmps/SubNote.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import { getNote } from "../services/apiService"
+
 
 export default function SubNote({ note }) {
     const [noteObj, setNoteObj] = useState(note)
@@ -11,6 +13,12 @@ export default function SubNote({ note }) {
         setNoteObj({ ...note, desc: ev.target.value })
     }
 
+    const handleBlur = async () => {
+        getNote(note.note_id)
+            .then((res) => { console.log(res) })
+            .catch((err) => { console.log(err) })
+    }
+
     if (note === undefined) {
         return (
             <div className="sub-note-container h-19 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
@@ -20,7 +28,7 @@ export default function SubNote({ note }) {
     }
     return (
         <div className="sub-note-container h-19 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
-            <input type="text" className="w-full h-full focus:outline-0 border-0" value={noteObj.desc} onChange={handleChange} onBlur={() => { console.log('blur') }} />
+            <input type="text" className="w-full h-full focus:outline-0 border-0" value={noteObj.desc} onChange={handleChange} onBlur={handleBlur} />
         </div>
     )
 }
--- services/apiService.js
+++ services/apiService.js
@@ -8,7 +8,17 @@ export const getNotes = async () => {
         const response = await axios.get(`${API_URL}/notes`)
         return response.data
     } catch (error) {
-        console.error("There was an error fetching the data:", error)
+        console.error("There was an error fetching notes:", error)
+        throw error
+    }
+}
+
+export const getNote = async (note_id) => {
+    try {
+        const response = await axios.get(`${API_URL}/notes/${note_id}`)
+        return response.data
+    } catch (error) {
+        console.error("There was an error fetching the note:", error)
         throw error
     }
 }
