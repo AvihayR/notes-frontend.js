cc1b6072da4e36e6ec8073e8b89559ed397008cf
AvihayR <avihayrabi@gmail.com>
2025-03-05 22:46:06 +0200

    Add checkbox for completing a note/task

--- cmps/SubNote.jsx
+++ cmps/SubNote.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { getNote, createNote, updateNote } from "../services/apiService"
 
 
-export default function SubNote({ note }) {
+export default function SubNote({ note, i }) {
     const [noteObj, setNoteObj] = useState(note)
     useEffect(() => {
         setNoteObj(note)
@@ -32,6 +32,15 @@ export default function SubNote({ note }) {
 
     }
 
+    const handleCheck = async () => {
+        setNoteObj({ ...noteObj, completed: !noteObj.completed })
+        await updateNote({
+            note_id: noteObj.note_id,
+            desc: noteObj.desc,
+            completed: !noteObj.completed
+        })
+    }
+
     if (note === undefined) {
         return (
             <div className="sub-note-container h-19 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
@@ -40,9 +49,9 @@ export default function SubNote({ note }) {
         )
     }
     return (
-        <div className="sub-note-container h-19 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
-            <input type="text" className="w-full h-full focus:outline-0 border-0" value={noteObj.desc} onChange={handleChange} onBlur={handleBlur} />
-            <label>{noteObj.note_id && '*'}</label>
+        <div className="sub-note-container relative flex justify-center items-center h-19 text-2xl font-light pl-18 text-blue-900 border-b-blue-200 border-b-1">
+            <input type="checkbox" name={i} onChange={handleCheck} checked={noteObj.completed} className="absolute left-5 mt-1.5 z-20 w-5 h-5 rounded-2xl accent-amber-400" />
+            <input type="text" className={`w-full h-full focus:outline-0 border-0 decoration-amber-400 ${noteObj.completed && 'line-through'}`} value={noteObj.desc} onChange={handleChange} onBlur={handleBlur} />
         </div>
     )
 }
